47b16d5dcf94de0247c5e4d9a30f0cb62485b47f
Daniel Hey <danieljameshey@outlook.com>
2024-04-25 14:46:41 +0100

    Updated Die.cs
    The die class has been updated and the code has been added.

--- Die.cs
+++ Die.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace OOP_Dice_Game
 {
-    internal class Class1
+    public class Die
     {
+        private Random random;  // Random number generator instance for rolling the die
+        public int FaceValue { get; private set; }  // Property to get the value of the die
+
+        // Constructor to initialize the die
+        public Die()
+        {
+            random = new Random();  // Initialize the random number generator
+            Roll(); // Roll the die to get its initial value of the die
+        }
+
+        // Method to roll the die
+        public int Roll()
+        {
+            FaceValue = random.Next(1, 7);  // Generate a random number between 1 and 6 (inclusive)
+            return FaceValue;    // Return the face value of the die
+        }
     }
 }
